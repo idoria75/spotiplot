b56613a7e08e2f35d1b34be2c3b13b9f523e770c
ivan <ivanpdoria@gmail.com>
2023-12-29 16:05:01 -0300

    Add Track class

--- monitor.py
+++ monitor.py
@@ -5,11 +5,32 @@ from yaml import safe_load
 
 PATH_TO_KEYS = '/usr/local/keys/spotiplot.env'
 
+## TODO
+## Store list of tracks
+## Store number of times played
+## Check if shuffle is on before storing new entries
+## Store continuously
+## Store songs played before and after an execution
+## Count number of tracks played by a single artist
+## Properly store tracks in a DB or log file (with parser)
+
+class Track():
+    def __init__(self, a_name='', a_artists=[]):
+        self.name = a_name
+        self.artists = ''
+        for i in range(len(a_artists) - 1):
+            self.artists = self.artists + a_artists[i]['name'] + ', '
+        self.artists = self.artists + a_artists[-1]['name']
+
+    def print(self):
+        print("{} by {}".format(self.name, self.artists))
+
 
 class Monitor():
 
     def __init__(self):
         self.authenticate()
+        self.last_played = []
 
     def authenticate(self):
         try:
@@ -38,18 +59,15 @@ class Monitor():
 
         tracks = []
 
-        for j in range(len(items)):
-            name = items[j]['track']['name']
-            artists = []
-            for i in range(len(items[j]['track']['artists'])):
-                artists.append(items[j]['track']['artists'][i]['name'])
-            tracks.append((name, artists))
+        for i in range(len(items)):
+            tracks.append(Track(a_name=items[i]['track']['name'], a_artists=items[i]['track']['artists']))
 
-        print(tracks)
+        for track in tracks:
+            track.print()
 
         # Get before and after unix timestamps
-        cursors = res_list['cursors']
-        print(cursors)
+        # cursors = res_list['cursors']
+        # print(cursors)
 
     def get_currently_playing(self):
         results = self.sp.current_playback()['item']['name']
