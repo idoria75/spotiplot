24523dfc09f21a5a6f84b8a1bf42d1af70caeef5
ivan <ivanpdoria@gmail.com>
2024-01-05 15:12:33 -0300

    Change table structure to reflect possible data to be stored

--- mysql/init-scripts/init.sql
+++ mysql/init-scripts/init.sql
@@ -1,14 +1,16 @@
 -- init.sql
-
 USE spotiplot;
 
-CREATE TABLE IF NOT EXISTS events (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    event_name VARCHAR(100),
-    event_details VARCHAR(255),
-    event_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Create the table
+CREATE TABLE listening_activity (
+    activity_id INT AUTO_INCREMENT PRIMARY KEY,
+    song_title VARCHAR(255),
+    artist VARCHAR(100),
+    album VARCHAR(100),
+    playback_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO events (event_name, event_details) VALUES ('Event 1', 'Details for Event 1');
-INSERT INTO events (event_name, event_details) VALUES ('Event 2', 'Details for Event 2');
--- Add more initialization data or schema changes as needed
+-- Insert sample data
+INSERT INTO listening_activity (song_title, artist, album) VALUES ('Song 1', 'Artist 1', 'Album 1');
+INSERT INTO listening_activity (song_title, artist, album) VALUES ('Song 3', 'Artist 2', 'Album 1');
+INSERT INTO listening_activity (song_title, artist, album) VALUES ('Song 2', 'Artist 3', 'Album X');
