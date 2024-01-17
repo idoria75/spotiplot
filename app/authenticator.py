788f8e1b60c012c8ee82c812ff2844c2d685371a
ivan <ivanpdoria@gmail.com>
2024-01-17 14:53:48 -0300

    Refactor based on black formatter

--- app/authenticator.py
+++ app/authenticator.py
@@ -2,30 +2,31 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from yaml import safe_load
 
-PATH_TO_KEYS = '/app/spotiplot.env'
+PATH_TO_KEYS = "/app/spotiplot.env"
 
 
-class Authenticator():
-
+class Authenticator:
     def __init__(self):
         print("Starting Spotiplot authenticator")
         try:
-            with open(PATH_TO_KEYS, 'r') as file:
-                vars = safe_load(file).get('credentials')
-                id = vars.get('client_id')
-                secret = vars.get('client_secret')
-                uri = vars.get('redirect_uri')
+            with open(PATH_TO_KEYS, "r") as file:
+                vars = safe_load(file).get("credentials")
+                id = vars.get("client_id")
+                secret = vars.get("client_secret")
+                uri = vars.get("redirect_uri")
 
-            self.scope = scope = "user-read-playback-state, user-read-currently-playing, user-read-recently-played"
+            scope = "user-read-playback-state, user-read-currently-playing, user-read-recently-played"
 
             ch = spotipy.CacheFileHandler("cache/.cache")
 
-            auth_manager = SpotifyOAuth(scope=scope,
-                                        client_id=id,
-                                        client_secret=secret,
-                                        redirect_uri=uri,
-                                        open_browser=False,
-                                        cache_handler=ch)
+            auth_manager = SpotifyOAuth(
+                scope=scope,
+                client_id=id,
+                client_secret=secret,
+                redirect_uri=uri,
+                open_browser=False,
+                cache_handler=ch,
+            )
 
             print("ID: {}\n Secret: {}\n URI: {}".format(id, secret, uri))
 
@@ -39,5 +40,5 @@ class Authenticator():
         print("Authentication succeeded")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     a = Authenticator()
